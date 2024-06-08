a1e859a7ccdab0aefd35b94b614454ec52534505
arthurmo <150570993+arthurmoo0605@users.noreply.github.com>
2024-06-08 19:45:40 -0400

    Update Logo.js

--- themes/fukasawa/components/Logo.js
+++ themes/fukasawa/components/Logo.js
@@ -6,7 +6,7 @@ const Logo = props => {
    <section className='flex'>
     <Link
      href='/'
-     className='hover:bg-black hover:text-white border-black border-2 duration-500 px-4 py-2 cursor-pointer dark:text-gray-300 dark:border-gray-300 font-black'>
+     className='hover:bg-black hover:text-white border-black border-0 duration-500 px-4 py-2 cursor-pointer dark:text-gray-300 dark:border-gray-300 font-black'>
      {siteConfig('TITLE')}
     </Link>
    </section>
